3c0785dbfbd48e256f1fdcbfefd5286cc87102a0
Bob Flanders <bob.flanders@gmail.com>
2024-06-30 11:03:24 -0400

    BF - Play with centurion assembler

---
+++ Software/New Software/UBanner/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace UBanner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("layout.txt");
+        int lineNum = 0;
+        Console.WriteLine("LAYOUT\tEQU\t*");
+        foreach (var line in lines)
+        {
+            char[] chars = line.ToCharArray();
+            for (int linePos = 0; linePos < chars.Length; linePos++)
+            {
+                if (chars[linePos] != ' ')
+                {
+                    Console.WriteLine($"\tDB\t{lineNum}");
+                    Console.WriteLine($"\tDB\t{linePos}");
+                    Console.WriteLine($"\tDB\t'{chars[linePos]}'");
+                    Console.WriteLine($"\tDB\t0");
+                    Console.WriteLine("*");
+                }
+            }
+            lineNum++;
+        }
+        Console.WriteLine("ELAYOUT\tEQU\t*");
+        Console.WriteLine("LOLEN\tEQU\t(*-LAYOUT)/4");
+    }
+}
--- Software/New Software/UBanner/ztst.asm
+++ Software/New Software/UBanner/ztst.asm
@@ -8,25 +8,42 @@ MUX0DATA  EQU       X'F201'        ; First MUX port data MMIO address.
 MUX0CB    EQU       X'C5'          ; First MUX port at 9,600 7E1.
 *
 * Entry point.
-ENTRY     XFR=      X'F000',S      ; Set the stack pointer to just below MMIO.
-* Initialize MUX ports
-          LDAB=     MUX0CB         ; Load Mux 0 Control Byte into A
-          STAB/     MUX0CTRL       ; Store A into MUX0CTRL, MMIO port for MUX0
-* Print "Hellorld!"
-* 8D = CR, 8A = LF, 8C = Clear screen
-          JSR/      PRINTNULL
-          DB        X'8C'
+MSG       DB        X'8C'
+          DB        X'1B'
+          DC        'Y'
+          DB        X'2A'
+          DB        X'32'
           DC        'HELLORLD!'
           DW        X'8D8A'
           DB        0              ; Null terminator
-* Print the null-terminated string at X to the CRT
-PRINTNULL STAB-     S-             ; Push AL to the stack
+
+ENTRY     EQU       *
+*         HLT
+          XFR=      X'F000',S      ; Set the stack pointer to just below MMIO.
+* Initialize MUX ports
+          LDAB=     MUX0CB         ; Load Mux 0 Control Byte into A
+          STAB/     MUX0CTRL       ; Store A into MUX0CTRL, MMIO port for MUX
+AGAIN     LDAB=     B'01'          ; A = 'Byte recieved?' mask
+          XAYB                     ; AL -> YL
+LOOPY     EQU       *
+          LDAB/     MUX0CTRL       ; al = MUX 0 control register
+          ANDB      YL,AL          ; q. byte received?
+          BZ        LOOPY          ; a. no .. loop until we get one.
+          LDAB/     MUX0DATA       ; AL = received data (who cares?) :D
+* Print "Hellorld!"
+* 8D = CR, 8A = LF, 8C = Clear screen
+          LDB=      MSG
+          XFR       B,Z
+          JSR/      PRTNULL
+          JMP/      AGAIN
+* Print the null-terminated string at Z to the CRT
+PRTNULL   STAB-     S-             ; Push AL to the stack
           STBB-     S-             ; Push BL to the stack
           XFRB      YL,AL          ; YL -> AL
           STAB-     S-             ; Push YL to the stack
           LDAB=     B'10'          ; Set mask to check for tx buffer empty
           XAYB                     ; AL -> YL
-PNLOOP    LDBB+     X+             ; Load the next byte
+PNLOOP    LDBB+     Z+             ; Load the next byte
           BZ        PNEND          ; If 0, we are done
 PNWAIT    LDAB/     MUX0CTRL       ; AL = MUX status byte
           ANDB      YL,AL          ; Check if transmit buffer empty
